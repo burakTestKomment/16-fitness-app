d99cc62503a5be5010d2e387a95c9c9bd0678855
bengin34 <becaglar3434@gmail.com>
2023-03-13 15:15:11 +0300

    half

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-
-const BodyPart = () => {
+import React from "react";
+import { Stack, Typography } from "@mui/material";
+import Icon from "../assets/icons/gym.png";
+import { borderTop } from "@mui/system";
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Stack
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="bodyPart-card"
+      sx={{
+        borderTop: bodyPart === item ? "4px solid #ff2625" : "",
+        backgroundColor: "#fff",
+        width: "270px",
+        height: "280px",
+        cursor: "pointer",
+        gap: "47px",
+      }}
+    >
+      <img src={Icon} alt="gym" styles={{ width: "40px", height: "40px" }} />
+    </Stack>
+  );
+};
 
-export default BodyPart
+export default BodyPart;
--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Box } from '@mui/material'
 import BodyPart from './BodyPart'
+import { ScrollMenu } from 'react-horizontal-scrolling-menu'
 
-const HorizontalScrollbar = ({data}) => {
+
+
+const HorizontalScrollbar = ({data, bodyPart, setBodyPart}) => {
   return (
-    <div>
+    <ScrollMenu>
       {data.map((item) => (<Box key={item.id || item}
-      itemId={item.id || item} title={item.id || item} m=' 0 40px'
+      itemID={item.id || item} title={item.id || item} m=' 0 40px'
       >
         <BodyPart  item={item} bodyPart = {bodyPart} setBodyPart={setBodyPart} />  
       </Box>)
       )}
-    </div>
+    </ScrollMenu>
   )
 }
 
