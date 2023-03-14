32ef2089a7090556be01e5269f40443cd582cc27
bengin34 <becaglar3434@gmail.com>
2023-03-14 11:57:38 +0300

    data

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,11 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { Pagination } from "@mui/material/Pagination";
 import { Box, Stack, Typography } from "@mui/material";
 import { exercisesOptions, fetchData } from "../utils/fetchData";
-import ExerciseCard from './ExerciseCard'
+import ExerciseCard from "./ExerciseCard";
+import Pagination from '@mui/material/Pagination';
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
-  console.log(exercises)
+ const [currentPage, setCurrentPage] = useState(1)
+ const exercisesPerPage = 9
+ const indexOfLastExercise = currentPage * exercisesPerPage
+ const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage
+ const currentExercises = exercises.slice(indexOfFirstExercise,indexOfLastExercise)
+ const paginate = (e,value) => {
+  setCurrentPage(value)
+  window.scrollTo({ top:1800 , behavior:'smooth' })
+ }
+ useEffect(() => {
+  const fetchExercisesData = async () =>{
+    let exercisesData = []
+
+    if(bodyPart === 'all'){
+      exercisesData =  await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises", exercisesOptions)
+    }else{
+
+    }
+  }
+ },[bodyPart])
   return (
     <Box
       id="exercises"
@@ -24,11 +44,22 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
           gap: { lg: "110px", xs: "50px" },
         }}
       >
-      {exercises.map((exercise, index) => ( 
-        <ExerciseCard key={index} exercise={exercise}/>
-         ))
-      }
-      
+        {currentExercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
       </Stack>
     </Box>
   );
